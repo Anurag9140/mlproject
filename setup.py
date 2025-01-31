24e76608faf5e74964d083b28946cc2a0d9c66f7
Anurag9140 <vijayverma4202@gmail.com>
2025-01-31 13:51:49 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='anurag',
+author_email='av3315740@gmail.com',
+packages=find_packages(),
+
+)
